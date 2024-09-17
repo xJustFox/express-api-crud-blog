fa240ebb144f48ff9165407fdf59571f5c689b37
xJustFox <dodydaversa@gmail.com>
2024-09-17 19:30:58 +0200

    Aggiunti middlewares errorsFormatter e routesNotFound

--- app.js
+++ app.js
@@ -9,9 +9,15 @@ const postsRouter = require('./routers/posts.js');
 app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+const errorsFormatter = require('./middlewares/errorsFormatter.js');
+const routesNotFound = require('./middlewares/routesNotFound.js');
 
 app.get('/', (req, res) => res.sendFile( path.join(__dirname,'./index.html')));
 
 app.use('/posts', postsRouter);
+
+
+app.use(routesNotFound);
+app.use(errorsFormatter);
 
 app.listen(port, () => console.log(`Example app listening on http://localhost:${port}`));
